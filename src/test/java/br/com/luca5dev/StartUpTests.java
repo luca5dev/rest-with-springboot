17f98884f0de4e8d936461e271432fc7037d4115
lucashenrique.info@gmail.com <lucashenrique.info@gmail.com>
2026-02-02 15:29:52 -0300

    first

---
+++ src/test/java/br/com/luca5dev/StartUpTests.java
@@ -0,0 +1,13 @@
+package br.com.luca5dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
