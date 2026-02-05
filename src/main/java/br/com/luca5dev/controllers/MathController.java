315789c75ad1069e18829737c3abfe1ba419e7e9
lucashenrique.info@gmail.com <lucashenrique.info@gmail.com>
2026-02-05 11:12:58 -0300

    exceptions custom 2

--- src/main/java/br/com/luca5dev/controllers/MathController.java
+++ src/main/java/br/com/luca5dev/controllers/MathController.java
@@ -1,5 +1,6 @@
 package br.com.luca5dev.controllers;
 
+import br.com.luca5dev.exception.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,40 @@ public class MathController {
     public Double sum(@PathVariable("numberOne") String numberOne,
                       @PathVariable("numberTwo") String numberTwo
     ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalAccessException();
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException("Please, set a numeric value!");
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
+    // http://localhost:8080/math/subtraction/3/5
+    @RequestMapping("/subtraction/{numberOne}/{numberTwo}")
+    public Double subtraction(@PathVariable("numberOne") String numberOne,
+                      @PathVariable("numberTwo") String numberTwo
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException("Please, set a numeric value!");
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    // http://localhost:8080/math/multiplication/3/5
+    @RequestMapping("/multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(@PathVariable("numberOne") String numberOne,
+                              @PathVariable("numberTwo") String numberTwo
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException("Please, set a numeric value!");
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    // http://localhost:8080/math/division/3/5
+    @RequestMapping("/division/{numberOne}/{numberTwo}")
+    public Double division(@PathVariable("numberOne") String numberOne,
+                              @PathVariable("numberTwo") String numberTwo
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException("Please, set a numeric value!");
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
 
     private Double convertToDouble(String strNumber) throws IllegalAccessException {
 
-        if (strNumber == null || strNumber.isEmpty())throw new IllegalAccessException();
+        if (strNumber == null || strNumber.isEmpty())throw new UnsupportedMathOperationException("Please, set a numeric value!");
         String number = strNumber.replace(",", ".");
         return Double.parseDouble(number);
     }
--- src/main/java/br/com/luca5dev/exception/handler/CustomEntityResponseHandler.java
+++ src/main/java/br/com/luca5dev/exception/handler/CustomEntityResponseHandler.java
@@ -1,6 +1,7 @@
 package br.com.luca5dev.exception.handler;
 
 import br.com.luca5dev.exception.ExceptionResponse;
+import br.com.luca5dev.exception.UnsupportedMathOperationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,9 +16,16 @@ import java.util.Date;
 @RestController
 public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler{
 
+    @ExceptionHandler(Exception.class)
     public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request){
         ExceptionResponse response = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
     return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UnsupportedMathOperationException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request){
+        ExceptionResponse response = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
 }
